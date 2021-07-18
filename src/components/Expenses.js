44814231eaa7eff0ff630f98a58ad096e9bc344d
Andrew Starlike <andrewstarlike@gmail.com>
2021-07-18 23:43:39 +0300

    Expenses components based on expenses object in App js

---
+++ src/components/Expenses.js
@@ -0,0 +1,23 @@
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+function Expenses(props) {
+  const expenses = props.expenses.map((element) => {
+    return (
+      <ExpenseItem
+        key={element.id}
+        title={element.title}
+        amount={element.amount}
+        date={element.date}
+      />
+    );
+  });
+
+  return (
+      <div className="expenses">
+          {expenses}
+      </div>
+  )
+}
+
+export default Expenses;
