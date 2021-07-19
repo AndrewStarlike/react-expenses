1a611fe591c574495e776290d321d23063267fca
Andrew Starlike <andrewstarlike@gmail.com>
2021-07-20 02:16:39 +0300

    made Expenses a controlled component because it controlls ExpensesFilter component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,16 @@
+import { useState } from "react";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   const expenses = props.expenses.map((element) => {
     return (
       <ExpenseItem
@@ -14,7 +22,15 @@ const Expenses = (props) => {
     );
   });
 
-  return <Card className="expenses">{expenses}</Card>;
-}
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      {expenses}
+    </Card>
+  );
+};
 
 export default Expenses;
